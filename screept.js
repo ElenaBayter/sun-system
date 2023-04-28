242d247eac9432dfe41ccff4d9ff7434eb46f3a1
elena tarasova <nafanichka.ev@gmail.com>
2023-04-28 14:18:09 -0500

    initial commit

---
+++ screept.js
@@ -0,0 +1,17 @@
+// let btnOpen = document.getElementById('myBtn');
+// let modal = document.getElementById('myModal');
+// let btnClose = document.querySelector('.close');
+
+// btnOpen.onclick = function(){
+// 	modal.style.display = 'block';
+// }
+// btnClose.onclick = function(){
+// 	modal.style.display = 'none';
+// }
+
+
+// let element = document.getElementById('myDIV');
+
+// function myFunction(){
+// 	element.classList.toggle('mystyle');
+// }
